99cce3942f0355c00d39b92e0d2dcf4312992f0d
JulesVautier <jules.vautier@epitech.eu>
2019-01-03 08:40:02 -0800

    thread

--- App/ocr_dispatcher/rpc_listener.py
+++ App/ocr_dispatcher/rpc_listener.py
@@ -2,26 +2,27 @@
 import json
 
 import pika
-import uuid
+import threading
 
-class ListenerRpc(object):
+class ListenerRpc(threading.Thread):
     def __init__(self):
+        threading.Thread.__init__(self)
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-
         self.channel = self.connection.channel()
         self.channel.basic_qos(prefetch_count=1)
         self.task_response = self.channel.queue_declare('rpc_response_queue', durable=True)
         self.channel.basic_consume(self.on_response, queue=self.task_response.method.queue, no_ack=True)
 
+
     def on_response(self, ch, method, props, body):
         self.response = body
         print('response : ',  self.response)
 
-    def start(self):
+    def run(self):
         print(' [x] Waiting for responses from workers')
         self.channel.start_consuming()
 
 
 listener_rpc = ListenerRpc()
-listener_rpc.start()
+listener_rpc.run()
 
