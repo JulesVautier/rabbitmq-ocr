1f86b7d5539b1fe6a0ea2ca5aaa28102c1016835
JulesVautier <jules.vautier@epitech.eu>
2019-01-03 02:10:16 -0800

    prefecth_count

--- new_task.py
+++ new_task.py
@@ -2,19 +2,17 @@
 import pika
 import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='new_queue', durable=True)
+channel.queue_declare(queue='task_queue', durable=True)
 
 message = ' '.join(sys.argv[1:]) or "Hello World!"
 channel.basic_publish(exchange='',
-                      routing_key="new_queue",
+                      routing_key='task_queue',
                       body=message,
                       properties=pika.BasicProperties(
-                          delivery_mode=2,  # make message persistent
+                         delivery_mode = 2, # make message persistent
                       ))
 print(" [x] Sent %r" % message)
-
 connection.close()
-
