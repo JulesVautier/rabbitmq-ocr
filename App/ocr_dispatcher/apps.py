1521bbfc71b01948cce18d5f3103c83bb77623c3
JulesVautier <jules.vautier@epitech.eu>
2019-01-03 07:46:51 -0800

    url path

--- App/ocr_dispatcher/apps.py
+++ App/ocr_dispatcher/apps.py
@@ -1,5 +1,10 @@
+import subprocess
+from time import sleep
+
 from django.apps import AppConfig
 
 
 class OcrDispatcherConfig(AppConfig):
     name = 'ocr_dispatcher'
+
+    # subprocess.call(['python', 'rpc_client.py'])
--- App/ocr_dispatcher/views.py
+++ App/ocr_dispatcher/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
+
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def ocr(request):
+    return HttpResponse(str(request))
